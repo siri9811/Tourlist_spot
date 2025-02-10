0d476d9cd05e857c0525436485aec5bcb9059e44
Duchul <guscjfqkr9811@gmail.com>
2025-02-10 16:06:16 +0900

    Initial commit

---
+++ src/main/java/com/example/Tourlist_spot/TourlistSpotApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tourlist_spot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourlistSpotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourlistSpotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Tourlist_spot/TourlistSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tourlist_spot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourlistSpotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
